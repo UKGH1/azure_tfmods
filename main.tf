673edd0594b3c9d02028ff5c383ffd681bcc6998
UdayKunta <105340365+UdayKunta@users.noreply.github.com>
2024-12-06 18:19:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
 module "storage_account" {
-  source = "./tfmodules/storage"
+  source                      = "./tfmodules/storage"
+  sa_name                     = var.sa_name 
+  sa_rgname                   = var.sa_rgname
+  sa_location                 = var.sa_location
+  sa_account_tier             = var.sa_account_tier
+  sa_account_replication_type = var.sa_account_replication_type
 }
