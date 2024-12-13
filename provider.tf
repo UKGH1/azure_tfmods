b9cc840efa0dc9a0ca50cacfbf17945e6134042c
UdayKunta <105340365+UdayKunta@users.noreply.github.com>
2024-12-13 16:01:16 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-fsapps-backstage"
-    storage_account_name = "devbackstagepoclogsmnfpq"
+    resource_group_name  = "fs-coe-proctor-test"
+    storage_account_name = "stgaccnttfstate01"
     container_name       = "tfstate-viabackstage"
     key                  = "stgacct.tfstate" //"${var.tfstate_file_name}"
   }
